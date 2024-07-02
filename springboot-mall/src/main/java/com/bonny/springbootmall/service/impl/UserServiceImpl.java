1d2183c8ae762e5bfb799175dc0ad601b62e5ace
Bonny <bonnywu2003@gmail.com>
2024-07-02 15:57:42 +0800

    完成【帳號】功能 - 加密密碼(MD5 Hash)、未完成的Test

--- springboot-mall/src/main/java/com/bonny/springbootmall/service/impl/UserServiceImpl.java
+++ springboot-mall/src/main/java/com/bonny/springbootmall/service/impl/UserServiceImpl.java
@@ -11,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
+import org.springframework.util.DigestUtils;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.server.ResponseStatusException;
 
@@ -34,6 +35,10 @@ public class UserServiceImpl implements UserService {
             log.warn("該 email 【{}】 已經被註冊", userRegisterRequest.getEmail());
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
         }
+        // 使用 MD5 Hash 生成雜湊值，將 String 轉換成 Byte 類型
+        String hashedPassword = DigestUtils.md5DigestAsHex(userRegisterRequest.getPassword().getBytes());
+        userRegisterRequest.setPassword(hashedPassword);
+
         // 創建帳號
         return userDao.createUser(userRegisterRequest);
     }
@@ -50,12 +55,17 @@ public class UserServiceImpl implements UserService {
         User user = userDao.getUserByEmail(userLoginRequest.getEmail());
 
 
+        // 檢查 user 是否存在
         if (user == null){
             log.warn("該 email 【{}】 尚未註冊", userLoginRequest.getEmail());
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
         }
-        // 要用 "equals" 比較字串
-        if (user.getPassword().equals(userLoginRequest.getPassword())){
+
+        // 使用 MD5 Hash 生成雜湊值，將 String 轉換成 Byte 類型
+        String hashedPassword = DigestUtils.md5DigestAsHex(userLoginRequest.getPassword().getBytes());
+
+        // 要用 "equals" 比較密碼
+        if (user.getPassword().equals(hashedPassword)){
             return user;
         } else {
             log.warn("該 email 【{}】 密碼不正確", userLoginRequest.getEmail());
--- springboot-mall/src/test/resources/schema.sql
+++ springboot-mall/src/test/resources/schema.sql
@@ -1,3 +1,14 @@
+-- 因為下方會使用 H2 資料庫，所以 "email" 以"UNIQUE" 表示"唯一"
+-- 如果使用 MySQL 資料庫， "email" 以"UNIQUE KEY" 表示"唯一"
+CREATE TABLE IF NOT EXISTS "user"
+(
+    user_id            INT          NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    email              VARCHAR(256) NOT NULL UNIQUE,
+    password           VARCHAR(256) NOT NULL,
+    created_date       TIMESTAMP    NOT NULL,
+    last_modified_date TIMESTAMP    NOT NULL
+    );
+
 CREATE TABLE IF NOT EXISTS product
 (
     product_id         INT          NOT NULL PRIMARY KEY AUTO_INCREMENT,
@@ -10,3 +21,9 @@ CREATE TABLE IF NOT EXISTS product
     created_date       TIMESTAMP    NOT NULL,
     last_modified_date TIMESTAMP    NOT NULL
     );
+
+
+
+
+
+
